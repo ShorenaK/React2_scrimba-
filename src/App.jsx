3415d486354cc8525cc93a60983dba775cff5591
ShorenaK <shorenaanzhilov@gmail.com>
2023-03-25 16:40:24 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react'
+import './App.css'
+import Main from './components/Main'
+import Navbar from './components/Navabar'
+
+
+function App() {
+
+  return (
+    <div className="App">
+   <Main/>
+   <Navbar/>
+   
+    </div>
+  )
+}
+
+export default App
